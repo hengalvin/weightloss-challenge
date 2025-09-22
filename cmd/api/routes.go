53965cbfa4e08242bcc5af0f835ffa55c1bf6191
Alvin Heng <alvin.heng@shopee.com>
2025-09-22 09:26:03 +0700

    allow cors from v0

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,7 +17,7 @@ func (app *application) routes() http.Handler {
 	// === CORS ===
 	g.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"https://v0-weight-loss-challenge-app.vercel.app",
+			"v0-weightloss-challenge-pi.vercel.app",
 			"http://localhost:3000", // dev
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
